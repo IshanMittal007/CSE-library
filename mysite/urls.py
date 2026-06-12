1e7d492526d855b95f06eb059c2e19b9b3e0ef37
IshanMittal007 <mittalf1@gmail.com>
2026-06-12 09:23:19 +0530

    Set default home page view

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,8 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('library', include('library.urls')),
-    path('/', user_views.register, name='register'),
-    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('register', user_views.register, name='register'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
 
